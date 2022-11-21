83a4239b81fccf76ad8c435da4ebe7b919b88a25
emilianospada <emilianos13@gmail.com>
2022-11-21 20:46:38 +0100

    Update settings.py

--- BASD/BASD/settings.py
+++ BASD/BASD/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k8m!upwb3ui0(h85yj_ghl6ps!ihws4075$m!^_rxm#owf7bx@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
